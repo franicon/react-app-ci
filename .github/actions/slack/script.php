8e7335df5bf9700fd0bba7865899ed96709be53c
Abayomi Aluko <franicons@gmail.com>
2023-02-08 15:34:36 +0100

    feat: create-slack-action-php

--- .github/actions/slack/script.php
+++ .github/actions/slack/script.php
@@ -6,4 +6,18 @@ Requests::register_autoloader();
 var_dump($argv);
 var_dump($_ENV);
 
+$response = Requests::post(
+    $_ENV['INPUT_SLACK_WEBHOOK']
+    array(
+       "content-type" => "application/json"
+    ),
+    json_encode (array(
+         'text' => $_ENV['INPUT_MESSAGE']
+    ))
+ );
 
+var_dump($response);
+
+if(!$response->success) {
+   echo $response->body;
+}
